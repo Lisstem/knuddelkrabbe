ea66538fe6077bea407e4edb156277650f2f5da0
lisstem <thole.goesmann@gmx.de>
2020-05-16 00:10:31 +0200

    Add impressum page

--- compile.rb
+++ compile.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'yaml'
 require 'tilt/haml'
 require 'tilt/sass'
 
@@ -8,11 +9,13 @@ Encoding.default_external = 'utf-8'
 def render(partial, content={}, &block)
   Tilt::HamlTemplate.new("src/#{partial}.haml").render(self, content, &block)
 end
+
+impressum_data = YAML.load_file('impressum_data.yaml')
 sites = ['index', 'impressum']
 template = Tilt.new('src/main.haml')
 sites.each do |site|
   html = File.open("html/#{site}.html", 'w')
-  html.puts template.render(self, site: site, construction: true)
+  html.puts template.render(self, site: site, construction: true, impressum: impressum_data)
   html.close
 end
 
