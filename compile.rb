8660e496b26e3d29b8487b39bf92e6a186a491c5
lisstem <thole.goesmann@gmx.de>
2020-05-15 22:27:35 +0200

    Add style sheet and compiler

--- compile.rb
+++ compile.rb
@@ -8,10 +8,15 @@ Encoding.default_external = 'utf-8'
 def render(partial, content={}, &block)
   Tilt::HamlTemplate.new("src/#{partial}.haml").render(self, content, &block)
 end
-sites = ['index']
+sites = ['index', 'impressum']
 template = Tilt.new('src/main.haml')
 sites.each do |site|
   html = File.open("html/#{site}.html", 'w')
   html.puts template.render(self, site: site, construction: true)
   html.close
 end
+
+template = Tilt.new('src/style.sass')
+css = File.open('css/style.css', 'w')
+css.puts template.render(self)
+css.close
