7c3b366e10ca63e8de5c93ce2ce7b56f6e60e120
lisstem <thole.goesmann@gmx.de>
2020-05-15 22:02:00 +0200

    Add templates for index and compiler

---
+++ compile.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'tilt/haml'
+
+Encoding.default_external = 'utf-8'
+
+def render(partial, content={}, &block)
+  Tilt::HamlTemplate.new("src/#{partial}.haml").render(self, content, &block)
+end
+sites = ['index']
+template = Tilt.new('src/main.haml')
+sites.each do |site|
+  html = File.open("html/#{site}.html", 'w')
+  html.puts template.render(self, site: site, construction: true)
+  html.close
+end
